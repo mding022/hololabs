f1f974fff7ede87652fa4336e76048d9fa001a86
millerding <mding022@uottawa.ca>
2024-08-22 14:41:10 -0400

    add various desktop animations

--- components/home.jsx
+++ components/home.jsx
@@ -65,7 +65,7 @@ export default function Component() {
     return (
         <div className="flex w-full flex-col bg-background relative font-sans">
             <header className="flex items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
-                <Link href="#" className="flex items-center" prefetch={false}>
+                <Link href="https://github.com/mding022/hololabs" className="flex items-center" prefetch={false}>
                     <Logo className="h-12 w-12" />
                     <span className="ml-2 text-lg font-medium">HoloLabs</span>
                 </Link>
@@ -129,6 +129,7 @@ function Logo(props) {
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            className="w-8 h-8 transform transition-transform duration-300 ease-in-out hover:scale-120 hover:rotate-90 cursor-pointer"
         >
             <path
                 d="M11 6H13V10.079L16.3413 7.73938L17.4885 9.37768L13.7434 12L17.4885 14.6223L16.3413 16.2606L13 13.921V18H11V13.921L7.65864 16.2606L6.51148 14.6223L10.2565 12L6.51147 9.37769L7.65863 7.73938L11 10.079V6Z"
